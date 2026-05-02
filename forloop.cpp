fcf7cbb59f00b71db91aebfda73b00e57e8f3ab9
Sumit <hplogin69@gmail.com>
2026-05-02 10:36:51 +0530

    Added new file forloop

---
+++ forloop.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+ int n;
+ cin>>n;
+ float x;
+ for (int i=1; i<=n; i++){
+    if (i%2==0){
+        cout<<i<<endl;
+    }
+}
+return 0;
+
+}
