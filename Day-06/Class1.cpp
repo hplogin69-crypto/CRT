0a00491288bb2a08751692b4e320c970a4267590
Sumit <hplogin69@gmail.com>
2026-05-06 09:49:14 +0530

    Update

--- Day-06/Class1.cpp
+++ Day-06/Class1.cpp
@@ -2,16 +2,20 @@
 using namespace std;
     class student {
         public:
-        int roll_no =1515;
-        string name ="Sumit" ;
+        // data members
+        int roll_no;
+        string name;
+        // member function
         void display () { 
         cout<<"Roll No. of student : " << roll_no<<endl;
         cout<<"Name of student is : " << name <<endl;
         
         }
     };
-    int main () {
+        int main () {
         student s1;
+        s1.roll_no = 1515;
+        s1.name = "Sumit";
         s1.display();
         return 0;
     }
