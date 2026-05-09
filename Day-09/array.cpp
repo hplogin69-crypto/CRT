ab84f3eca6d9eabae767d8d1b05157dab3502455
Sumit <hplogin69@gmail.com>
2026-05-09 09:45:51 +0530

    1-D array with calculation of size

---
+++ Day-09/array.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+    int main (){
+        int n;
+        int size;
+        cout<<"Enter the no. of elements u want in an array :"<<endl;
+        cin >> n;
+        int arr[n] = {1, 2, 3, 4, 5};
+        for (int i = 0; i < 5; i++){
+            cout << arr[i] << " ";
+        }
+        size = sizeof (arr)/sizeof (int);
+        cout << endl;
+        cout<<size;
+        return 0;
+    }
